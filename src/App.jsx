cf95cebb48c5e51d7cd835f8ff5b7fd3ec9a2852
VAISHNUPRIYA-V <vaishnupriya0811@gmail.com>
2025-07-16 07:52:33 +0530

    signup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+function App() {
+  return(
+    <div>
+      <Login />
+      <Signup/>
+    </div>
+  )
+}
+
+export default App
